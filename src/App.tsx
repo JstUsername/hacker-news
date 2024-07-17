89c2c4f66938f5969818024a44cbc1a782ce4651
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-17 17:18:35 +0300

    fix(App): реализован роутинг с помощью React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from './theme.ts';
-import Header from './components/Header/Header.tsx';
-import NewsList from './components/NewsList/NewsList.tsx';
-import Footer from './components/Footer/Footer.tsx';
+import Layout from './components/Layout/Layout.tsx';
+import HomePage from './pages/HomePage/HomePage.tsx';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <NewsList />
-      <Footer />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
