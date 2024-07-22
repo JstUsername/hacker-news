9fcc044780a8dc8ff439f297b8aa5a6003d23fa0
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-22 20:29:12 +0300

    feat(updateCurrentNews): в зависимости от пути, в header, разные функции обновления

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
 import { StyledHeader, LinkWrapper, ButtonLink, StyledLink, HNLogo, CroppedHNLogo } from './Header.styled.ts';
-import { useNewsListState } from '../../store/store.ts';
-import { useNavigate } from 'react-router-dom';
+import { useNewsItemState, useNewsListState } from '../../store/store.ts';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function Header() {
   const { updateNewsList } = useNewsListState();
   const navigate = useNavigate();
+  const { getNewsItem } = useNewsItemState();
+  const { newsList } = useNewsListState();
+  const { id } = useParams();
 
   return (
     <StyledHeader>
       <CroppedHNLogo title="Logo" />
       <HNLogo title="Logo" onClick={() => navigate('/')} />
       <LinkWrapper>
-        <ButtonLink onClick={() => updateNewsList(false)}>refreshNewsList()</ButtonLink>
+        <ButtonLink
+          onClick={() => (id === undefined ? updateNewsList(false) : getNewsItem(Number(id), newsList, false))}
+        >
+          {id === undefined ? 'refreshNewsList()' : 'updateCurrentNews()'}
+        </ButtonLink>
         <StyledLink href="https://github.com/JstUsername">/github</StyledLink>
         <StyledLink href="https://t.me/JstUser">/telegram</StyledLink>
         <StyledLink href="https://github.com/tastejs/hacker-news-pwas/">/api</StyledLink>
