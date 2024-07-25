7d46f0f72308670e667dd04aab75eec386dbd757
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-25 23:42:25 +0300

    pr(CroppedHNLogo, HNLogo): логотипы обернуты в тэг Link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,19 @@
 import { StyledHeader, LinkWrapper, ButtonLink, StyledLink, HNLogo, CroppedHNLogo } from './Header.styled.ts';
 import { useSelectorGetNewsItem, useSelectorNewsList, useSelectorUpdateNewsList } from '../../store/store.ts';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 export default function Header() {
   const newsList = useSelectorNewsList();
   const getNewsItem = useSelectorGetNewsItem();
   const updateNewsList = useSelectorUpdateNewsList();
-  const navigate = useNavigate();
   const { id } = useParams();
 
   return (
     <StyledHeader>
-      <CroppedHNLogo title="Logo" onClick={() => navigate('/')} />
-      <HNLogo title="Logo" onClick={() => navigate('/')} />
+      <Link to="/">
+        <CroppedHNLogo title="Logo" />
+        <HNLogo title="Logo" />
+      </Link>
       <LinkWrapper>
         <ButtonLink
           onClick={() => (id === undefined ? updateNewsList(false) : getNewsItem(Number(id), newsList, false))}
