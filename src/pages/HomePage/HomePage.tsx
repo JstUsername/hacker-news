d66b39faeabcfb75787a0cac371ca9c0fbf8c30a
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-29 04:43:32 +0300

    pr(ErrorBoundary): NewsList обернут в ErrorBoundary

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,11 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import NewsList from '../../components/NewsList/NewsList.tsx';
+import InternalServerError from '../../components/InternalServerError/InternalServerError.tsx';
 
 export default function HomePage() {
-  return <NewsList />;
+  return (
+    <ErrorBoundary fallback={<InternalServerError />}>
+      <NewsList />
+    </ErrorBoundary>
+  );
 }
