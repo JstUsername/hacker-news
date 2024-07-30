e1744f20d16f66b4287b430c00ad98b06b79b7a9
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-30 04:08:05 +0300

    pr(App): ErrorBoundary вынесен в App, использован FallbackComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelectorResetNotFoundPage } from './store/store.ts';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from './theme.ts';
+import { ErrorBoundary } from 'react-error-boundary';
 import Layout from './components/Layout/Layout.tsx';
 import HomePage from './pages/HomePage/HomePage.tsx';
 import NewsItemPage from './pages/NewsItemPage/NewsItemPage.tsx';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
+import ErrorHandler from './components/ErrorHandler/ErrorHandler.tsx';
 
 function App() {
+  const resetNotFoundPage = useSelectorResetNotFoundPage();
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/news/:id" element={<NewsItemPage />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route
+              index
+              element={
+                <ErrorBoundary FallbackComponent={ErrorHandler}>
+                  <HomePage />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="/news/:id"
+              element={
+                <ErrorBoundary FallbackComponent={ErrorHandler} onReset={resetNotFoundPage}>
+                  <NewsItemPage />
+                </ErrorBoundary>
+              }
+            />
+            <Route
+              path="*"
+              element={<ErrorHandler error={new Error('Page not found')} resetErrorBoundary={() => undefined} />}
+            />
           </Route>
         </Routes>
       </ThemeProvider>
