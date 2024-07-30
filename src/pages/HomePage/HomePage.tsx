4c26343bf0a01bc966cb2b30d86b67e6a2a6a1e5
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-30 04:03:43 +0300

    fix(HomePage): ErrorBoundary удален из HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,5 @@
-import { ErrorBoundary } from 'react-error-boundary';
 import NewsList from '../../components/NewsList/NewsList.tsx';
-import InternalServerError from '../../components/InternalServerError/InternalServerError.tsx';
 
 export default function HomePage() {
-  return (
-    <ErrorBoundary fallback={<InternalServerError />}>
-      <NewsList />
-    </ErrorBoundary>
-  );
+  return <NewsList />;
 }
