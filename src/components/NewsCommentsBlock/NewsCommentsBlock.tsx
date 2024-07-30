c7cb82c7af8a8029f71a6648f7499a4bcab678a1
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-07-30 15:42:54 +0300

    fix(NewsCommentsBlock): удален Suspense, использован условный рендеринг

--- src/components/NewsCommentsBlock/NewsCommentsBlock.tsx
+++ src/components/NewsCommentsBlock/NewsCommentsBlock.tsx
@@ -1,12 +1,12 @@
-import { lazy, Suspense } from 'react';
-import { useSelectorNewsItem } from '../../store/store.ts';
+import { useSelectorCommentsLoading, useSelectorNewsItem } from '../../store/store.ts';
 import { Loader } from '../NewsList/NewsList.styled.ts';
 import { CommentsWrapper, CommentsLoaderWrapper } from './NewsCommentsBlock.styled.ts';
+import CommentsItem from '../CommentsItem/CommentsItem.tsx';
 import { NewsItemTitle, NewsItemTitleAdditional } from '../../commons/NewsItemTitle.ts';
 
 export default function NewsCommentsBlock() {
-  const CommentsItem = lazy(() => import('../CommentsItem/CommentsItem.tsx'));
   const newsItem = useSelectorNewsItem();
+  const commentsLoading = useSelectorCommentsLoading();
 
   return (
     newsItem !== null && (
@@ -15,17 +15,13 @@ export default function NewsCommentsBlock() {
           Comments
           <NewsItemTitleAdditional>{' ' + newsItem.comments_count}</NewsItemTitleAdditional>
         </NewsItemTitle>
-        <Suspense
-          fallback={
-            <CommentsLoaderWrapper>
-              <Loader />
-            </CommentsLoaderWrapper>
-          }
-        >
-          {newsItem.comments.map((comment) => (
-            <CommentsItem key={comment.id} comment={comment} />
-          ))}
-        </Suspense>
+        {!commentsLoading ? (
+          newsItem.comments.map((comment) => <CommentsItem key={comment.id} comment={comment} />)
+        ) : (
+          <CommentsLoaderWrapper>
+            <Loader />
+          </CommentsLoaderWrapper>
+        )}
       </CommentsWrapper>
     )
   );
