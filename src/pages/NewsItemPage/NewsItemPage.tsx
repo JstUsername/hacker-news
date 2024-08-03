f78d86c4c33ed005f2634baed67e962ad5ca97cf
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-03 19:32:26 +0300

    pr(NewsItemPage): удалены lazy и newsItem

--- src/pages/NewsItemPage/NewsItemPage.tsx
+++ src/pages/NewsItemPage/NewsItemPage.tsx
@@ -1,18 +1,16 @@
-import { useEffect, useRef, Suspense, lazy } from 'react';
+import { useEffect, useRef, Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 import {
   useSelectorGetNewsItem,
   useSelectorItemServerDown,
   useSelectorItemPageNotFound,
-  useSelectorNewsItem,
 } from '../../store/states/newsItemState/newsItemState.ts';
+import NewsItemWrapper from './NewsItemPage.styled.ts';
 import NewsContentBlock from '../../components/NewsContentBlock/NewsContentBlock.tsx';
 import NewsCommentsBlock from '../../components/NewsCommentsBlock/NewsCommentsBlock.tsx';
 import { Loader, LoaderWrapper } from '../../components/NewsList/NewsList.styled.ts';
-const NewsItemWrapper = lazy(() => import('./NewsItemPage.styled.ts'));
 
 export default function NewsItemPage() {
-  const newsItem = useSelectorNewsItem();
   const itemServerDown = useSelectorItemServerDown();
   const itemPageNotFound = useSelectorItemPageNotFound();
   const getNewsItem = useSelectorGetNewsItem();
@@ -24,7 +22,7 @@ export default function NewsItemPage() {
       getNewsItem(Number(id));
       hasMounted.current = true;
     }
-  }, [getNewsItem, id, newsItem]);
+  }, [getNewsItem, id]);
 
   useEffect(() => {
     const autoUpdateInterval = setInterval(() => getNewsItem(Number(id)), 60000);
