7fe756db0830c75965f84e177f82581cf825b415
Andrey Gorchakov <g4ndy_c4ndy@mail.ru>
2024-08-03 05:24:24 +0300

    pr(NewsCommentsBlock): добавлен локальный стейт и функция для добавления полей

--- src/components/NewsCommentsBlock/NewsCommentsBlock.tsx
+++ src/components/NewsCommentsBlock/NewsCommentsBlock.tsx
@@ -1,10 +1,24 @@
+import { useCallback, useEffect, useState } from 'react';
 import { useSelectorNewsItem } from '../../store/states/newsItemState/newsItemState.ts';
 import { CommentsWrapper } from './NewsCommentsBlock.styled.ts';
 import CommentsItem from '../CommentsItem/CommentsItem.tsx';
 import { NewsItemTitle, NewsItemTitleAdditional } from '../../commons/NewsItemTitle/NewsItemTitle.ts';
+import { NewsItemType } from '../../store/states/newsItemState/newsItemState.types.ts';
 
 export default function NewsCommentsBlock() {
   const newsItem = useSelectorNewsItem();
+  const [commentItems, setCommentItems] = useState<NewsItemType[] | null>(null);
+
+  const addFieldsToComments = useCallback((comments: NewsItemType[]) => {
+    return comments.map((comment) => {
+      comment.visible = false;
+      comment.expand = false;
+      if (comment.comments && comment.comments.length > 0) {
+        comment.comments = addFieldsToComments(comment.comments);
+      }
+      return comment;
+    });
+  }, []);
 
   return (
     newsItem !== null && (
