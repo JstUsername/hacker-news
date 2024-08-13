639b5ff9dc12d6d5d6ef37e9bf7b2d0a714aec06
Андрей Горчаков <g4ndy_c4ndy@mail.ru>
2024-08-13 21:13:18 +0300

    pr(Text): исправлена типизация пропов $variant: и color

--- src/commons/Text/Text.ts
+++ src/commons/Text/Text.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { TextProps } from './Text.types';
 
-export const Text = styled('span')<{ $variant: string; color: string }>`
+export const Text = styled('span')<TextProps>`
   ${({ theme, $variant }) => theme.typography[$variant]};
   color: ${({ theme, color }) => theme.color[color]};
 `;
---
+++ src/commons/Text/Text.types.ts
@@ -0,0 +1,4 @@
+export interface TextProps {
+  $variant: 'h1' | 'body' | 'body2';
+  color: 'background' | 'backgroundLightTransparent' | 'backgroundDark' | 'purple' | 'yellow' | 'green';
+}
