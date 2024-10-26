5e76b4fafcd2b27fe957ef3f1905a13bd285e316
Андрей Горчаков <g4ndy_c4ndy@mail.ru>
2024-10-27 02:47:47 +0300

    feat(turborepo, express, sequelize, postgresql, husky): реализован монорепозиторий, добавлена основа для серверной части, перенастроены конфигурации eslint, prettier, typescript, добавлен docker-compose с БД

---
+++ app/frontend/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['plugin:@web-bee-ru/base', 'plugin:@web-bee-ru/react', '../../.eslintrc.cjs'],
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ app/frontend/src/commons/Text/Text.types.ts
@@ -0,0 +1,4 @@
+export interface TextProps {
+  $variant: 'h1' | 'body1' | 'body2';
+  color: 'background' | 'backgroundLightTransparent' | 'backgroundDark' | 'purple' | 'yellow' | 'green';
+}
