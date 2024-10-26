342ce6debcf70584c9144a03c93356727a83a3cb
Андрей Горчаков <g4ndy_c4ndy@mail.ru>
2024-10-27 02:47:47 +0300

    feat(turborepo, express, sequelize, postgresql, husky): реализован монорепозиторий, добавлена основа для серверной части, перенастроены конфигурации eslint, prettier, typescript, добавлен docker-compose с БД

--- app/frontend/src/commons/Loader/Loader.ts
+++ app/frontend/src/commons/Loader/Loader.ts
@@ -1,5 +1,33 @@
+<<<<<<<< HEAD:app/frontend/src/commons/Loader/Loader.ts
 import styled from 'styled-components';
 import HourGlass from '~/assets/icons/hour-glass-icon.svg?react';
+========
+import HourGlass from '../../assets/hour-glass.svg?react';
+import styled from 'styled-components';
+
+export const NewsListWrapper = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 24px;
+  gap: 18px;
+  box-sizing: border-box;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    min-height: ${({ theme }) => theme.heights.mainContentDesktop};
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    min-height: ${({ theme }) => theme.heights.mainContentMobile};
+    grid-template-columns: repeat(1, 1fr);
+    padding: 16px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+>>>>>>>> 0bb5e18 (feat(turborepo, express, sequelize, postgresql, husky): реализован монорепозиторий, добавлена основа для серверной части, перенастроены конфигурации eslint, prettier, typescript, добавлен docker-compose с БД):app/frontend/src/components/NewsList/NewsList.styled.ts
 
 export const LoaderWrapper = styled('div')`
   display: flex;
