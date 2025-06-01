69f80046bff959b167ae7908d2355cf90bf180d5
Андрей Горчаков <agorchakov@web-bee.ru>
2025-06-01 04:11:03 +0300

    feat(items, seeders): добавлена сущность Items, реализованы ендпоинты, сервисные функции, сидеры, подключен cors

--- app/frontend/src/store/states/newsItemState/newsItemState.ts
+++ app/frontend/src/store/states/newsItemState/newsItemState.ts
@@ -3,6 +3,9 @@ import { create } from 'zustand';
 import { NOT_FOUND_ERROR, WENT_WRONG_ERROR } from '~/constants';
 import { createEmptyPromise } from '~/utils';
 
+const EXPRESS_HOST = import.meta.env.VITE_EXPRESS_HOST || 'localhost';
+const EXPRESS_PORT = parseInt(import.meta.env.VITE_EXPRESS_PORT || '3001');
+
 const fetchNewsItem = async (id: number) => {
   let data: NewsItemType | null = null;
   const response = await fetch(`/api/items/${id}`);
