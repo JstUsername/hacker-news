70ec18baf7b1d11ffa94c92c3f51d675e48a4ef1
Андрей Горчаков <agorchakov@web-bee.ru>
2025-06-07 02:54:20 +0300

    refactor: обновлены пакеты, скорректированы конфигурации, добавлены команды, исправлены недочеты в работе frontend

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -9,6 +9,4 @@ module.exports = {
   bracketSpacing: true,
   arrowParens: 'always',
   endOfLine: 'lf',
-  importOrderParserPlugins: ['typescript', 'decorators-legacy'],
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
 };
